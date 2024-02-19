1006179c6c76cd10c609b2c7102197c7afaa8e0c
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 13:46:27 -0800

    Add files via upload

---
+++ Cities.js
@@ -0,0 +1,12 @@
+"use strict";
+//Written by : TAYYABA RAO
+//Date: 18/feb/2024
+//Roll no : 00388416
+// set describe city function
+function describe_city(cityName, country = 'DefualtCountry') {
+    console.log(`${cityName} is in ${country}.`);
+}
+//result
+describe_city("Karachi", "Pakistan");
+describe_city("Tokyo", "Japan");
+describe_city("Paris");
---
+++ Cities.ts
@@ -0,0 +1,13 @@
+//Written by : TAYYABA RAO
+//Date: 18/feb/2024
+//Roll no : 00388416
+
+// set describe city function
+function describe_city(cityName: string, country: string = 'DefualtCountry'){
+    console.log(`${cityName} is in ${country}.`);
+}
+
+//result
+describe_city("Karachi","Pakistan");
+describe_city("Tokyo", "Japan");
+describe_city("Paris");
